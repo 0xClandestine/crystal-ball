a0678d84c74cf919f0463c9a711784e4abfc1d9f
0xClandestine <clandestine.eth@proton.me>
2023-06-02 15:28:13 -0500

    ✨ feat: add `CrystallBall` deployment script

--- test/SimpleAddition.t.sol
+++ test/SimpleAddition.t.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/LibCrystalBall.sol";
-import "./SimpleAddition.sol";
 
 contract SimpleAddition {
     function add(uint256 a, uint256 b) external pure returns (uint256) {
