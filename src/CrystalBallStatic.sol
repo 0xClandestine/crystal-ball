1aa68c0983062a80786532a365b04eef3e324f9d
0xClandestine <clandestine.eth@proton.me>
2023-06-03 03:53:15 -0500

    ✨ feat: add static vevm wrapper

---
+++ src/CrystalBallStatic.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract CrystalBallStatic {
+    address public immutable vevm;
+
+    constructor(address _vevm) {
+        vevm = _vevm;
+    }
+
+    error BadDelegatecall();
+
+    function delegatecall(bytes calldata callData)
+        external
+        virtual
+        returns (bytes memory)
+    {
+        if (msg.sender != address(this)) revert();
+
+        (bool success, bytes memory data) = vevm.delegatecall(callData);
+
+        if (!success) revert BadDelegatecall();
+
+        return data;
+    }
+
+    error BadStaticcall();
+
+    fallback(bytes calldata callData) external virtual returns (bytes memory) {
+        (bool success, bytes memory data) = address(this).staticcall(
+            abi.encodeCall(this.delegatecall, callData)
+        );
+
+        if (!success) revert BadStaticcall();
+
+        return abi.decode(data, (bytes));
+    }
+}
