4d5da103e131783023f67beb42970dde96ac8f38
0xClandestine <clandestine.eth@proton.me>
2023-06-04 22:11:25 -0500

    ♻️ refactor: optimize `replaceFirst`

--- src/Magic.sol
+++ src/Magic.sol
@@ -56,50 +56,83 @@ library Magic {
     }
 
     function replaceFirst(
-        bytes memory data,
-        bytes memory replace,
-        bytes memory with
-    ) internal pure returns (bytes memory output) {
-        unchecked {
-            uint256 dataLen = data.length;
-            uint256 replaceLen = replace.length;
-            uint256 withLen = with.length;
+        bytes memory subject,
+        bytes memory search,
+        bytes memory replacement
+    ) internal pure returns (bytes memory result) {
+        assembly {
+            let subjectLength := mload(subject)
+            let searchLength := mload(search)
+            let replacementLength := mload(replacement)
 
-            if (replaceLen == 0 || dataLen < replaceLen) {
-                return data;
-            }
+            // Store the mask for sub-word comparisons in the scratch space.
+            mstore(0x00, not(0))
+            mstore(0x20, 0)
 
-            output = new bytes(dataLen - replaceLen + withLen);
+            subject := add(subject, 0x20)
+            search := add(search, 0x20)
+            replacement := add(replacement, 0x20)
+            result := add(mload(0x40), 0x20)
 
-            for (uint256 i; i <= dataLen - replaceLen; ++i) {
-                bool matches = true;
+            let k := 0
 
-                for (uint256 j; j < replaceLen; ++j) {
-                    if (data[i + j] != replace[j]) {
-                        matches = false;
-                        break;
+            let subjectEnd := add(subject, subjectLength)
+            if iszero(gt(searchLength, subjectLength)) {
+                let subjectSearchEnd := add(sub(subjectEnd, searchLength), 1)
+                for {} lt(subject, subjectSearchEnd) {} {
+                    let o := and(searchLength, 31)
+                    // Whether the first `searchLength % 32` bytes of
+                    // `subject` and `search` matches.
+                    let l :=
+                        iszero(
+                            and(
+                                xor(mload(subject), mload(search)),
+                                mload(sub(0x20, o))
+                            )
+                        )
+                    // Iterate through the rest of `search` and check if any word mismatch.
+                    // If any mismatch is detected, `l` is set to 0.
+                    for {} and(lt(o, searchLength), l) {} {
+                        l := eq(mload(add(subject, o)), mload(add(search, o)))
+                        o := add(o, 0x20)
+                    }
+                    // If `l` is one, there is a match, and we have to copy the `replacement`.
+                    if l {
+                        // Copy the `replacement` one word at a time.
+                        for { o := 0 } lt(o, replacementLength) {
+                            o := add(o, 0x20)
+                        } {
+                            mstore(
+                                add(result, add(k, o)),
+                                mload(add(replacement, o))
+                            )
+                        }
+                        k := add(k, replacementLength)
+                        subject := add(subject, searchLength)
+                        // Stop after replacing the first instance.
+                        break
+                    }
+                    // If `l` or `searchLength` is zero.
+                    if iszero(mul(l, searchLength)) {
+                        mstore(add(result, k), mload(subject))
+                        k := add(k, 1)
+                        subject := add(subject, 1)
                     }
                 }
-
-                if (matches) {
-                    // Copy the data before the replaced portion
-                    for (uint256 k; k < i; ++k) {
-                        output[k] = data[k];
-                    }
-
-                    // Copy the replacement
-                    for (uint256 k; k < withLen; ++k) {
-                        output[i + k] = with[k];
-                    }
-
-                    // Copy the remaining data after the replaced portion
-                    for (uint256 k = i + replaceLen; k < dataLen; ++k) {
-                        output[k + withLen - replaceLen] = data[k];
-                    }
-
-                    break;
-                }
             }
+
+            let resultRemainder := add(result, k)
+            k := add(k, sub(subjectEnd, subject))
+            // Copy the rest of the string one word at a time.
+            for {} lt(subject, subjectEnd) {} {
+                mstore(resultRemainder, mload(subject))
+                resultRemainder := add(resultRemainder, 0x20)
+                subject := add(subject, 0x20)
+            }
+            // Allocate memory for the length and the bytes, rounded up to a multiple of 32.
+            mstore(0x40, add(result, and(add(k, 64), not(31))))
+            result := sub(result, 0x20)
+            mstore(result, k)
         }
     }
 }
